934346e86f99f33bbab008f3f0312e41748f8525
liam <liam@nehoray.net>
2024-10-06 11:42:15 +0200

    Minimal viable project

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const path = require("node:path");
-
 // Express
 const express = require("express");
 const app = express();
+// Routes
+const indexRouter = require('./routes/indexRouter')
+
+app.use(express.urlencoded({ extended: true }));
 
 // Views
+const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// Routes
-const indexRouter = require('./routes/indexRouter')
-const exampleRouter = require('./routes/exampleRoute')
 
 app.use('/', indexRouter)
-app.use('/example', exampleRouter)
 
 // Tell app to listen on PORT
 const PORT = process.env.PORT || 3000;
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,9 +1,39 @@
 const { Router } = require("express");
+const formRouter = require("./formRoute");
 
 const indexRouter = Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+indexRouter.use('/new', formRouter)
+
 indexRouter.get("/", (req, res) => {
-    res.render("index", { message: "Index" })
+  res.render("messages", { title: "Mini Message Board", messages: messages })
 });
+
+indexRouter.get('/message/:id', (req, res) => {
+  const message = messages[req.params.id]
+  res.render('message', { title: 'Message Details', message: message });
+})
+
+indexRouter.post('/new', (req, res) => {
+  messages.push({
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date()
+  })
+  res.send('   <p>Message sent!</p> <a href="http://localhost:3000/">Home</a> ')
+})
 
 module.exports = indexRouter;
